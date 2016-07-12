aca050186a35cd0cc30394e7f01cf87b32166a73
mikesnchz <mrbsan@gmail.com>
2016-07-12 18:54:39 +0800

    Test commit

---
+++ greeter.rb
@@ -0,0 +1,19 @@
+def greeter(name)
+    return "Hello, #{name}!"
+end
+
+def by_three?(x)
+    return x % 3 == 0
+end
+
+puts "Enter name:"
+my_name = gets.chomp
+output = greeter(my_name)
+puts output
+
+puts "Enter #:"
+num = gets.chomp
+output = by_three?(num.to_i)
+puts output
+
+end
