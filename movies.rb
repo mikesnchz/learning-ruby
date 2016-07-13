bb7d5d380b49bc6e45dce9bcf81f29701156d7ab
mikesnchz <mrbsan@gmail.com>
2016-07-13 15:25:21 +0800

    new movies.rb

--- movies.rb
+++ movies.rb
@@ -1,52 +1,66 @@
 movies = {
-  Memento: 3,
-  Primer: 4,
-  Ishtar: 1
+    :Titanic => 4,
+    :"The Big Hit" => 2,
+    :"Finding Dory" => 4,
+    :"John Wick" => 4
 }
 
-puts "What would you like to do?"
-puts "-- Type 'add' to add a movie."
-puts "-- Type 'update' to update a movie."
-puts "-- Type 'display' to display all movies."
-puts "-- Type 'delete' to delete a movie."
-
+puts "Please choose an option:"
+puts "-- add"
+puts "-- update"
+puts "-- display"
+puts "-- delete"
 choice = gets.chomp.downcase
+
 case choice
-when 'add'
-  puts "What movie do you want to add?"
-  title = gets.chomp
-  if movies[title.to_sym].nil?
-    puts "What's the rating? (Type a number 0 to 4.)"
+when "add"
+    puts "Please enter the title of the movie:"
+    title = gets.chomp
+    title = title.gsub(/\w+/){|word| word.capitalize}
+    puts "Please enter a rating for the movie (0 to 4):"
     rating = gets.chomp
-    movies[title.to_sym] = rating.to_i
-    puts "#{title} has been added with a rating of #{rating}."
-  else
-    puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
-  end
-when 'update'
-  puts "What movie do you want to update?"
-  title = gets.chomp
-  if movies[title.to_sym].nil?
-    puts "Movie not found!"
-  else
-    puts "What's the new rating? (Type a number 0 to 4.)"
-    rating = gets.chomp
-    movies[title.to_sym] = rating.to_i
-    puts "#{title} has been updated with new rating of #{rating}."
-  end
-when 'display'
-  movies.each do |movie, rating|
-    puts "#{movie}: #{rating}"
-  end
-when 'delete'
-  puts "What movie do you want to delete?"
-  title = gets.chomp
-  if movies[title.to_sym].nil?
-    puts "Movie not found!"
-  else
-    movies.delete(title.to_sym)
-    puts "#{title} has been removed."
-  end
+    if movies[title.to_sym].nil?
+        movies[title.to_sym] = rating.to_i
+        puts "#{title} has been added with a rating of #{rating}."
+    else
+        puts "#{title} already exists in the database."
+    end
+
+when "update"
+    puts "Please enter the title of the movie:"
+    title = gets.chomp
+    title = title.gsub(/\w+/){|word| word.capitalize}
+    if movies[title.to_sym].nil?
+        puts "Movie does not exist in the database."
+    else
+        puts "Please enter a new rating for the movie, #{title}:"
+        rating = gets.chomp
+        puts movies[title.to_sym]
+        movies[title.to_sym] = rating.to_i
+        puts "#{title}'s rating has been updated."
+    end
+
+when "display"
+    movies.each do |movie, rating|
+        puts "#{movie}: #{rating}"
+    end
+
+when "delete"
+    puts "Please enter the title of the movie you want to remove from the database:"
+    title = gets.chomp
+    title = title.gsub(/\w+/){|word| word.capitalize}
+    if movies[title.to_sym].nil?
+        puts "Movie does not exist in the database."
+    else
+        movies.delete(title.to_sym)
+        puts "#{title} has been deleted from the database."
+        puts "The remaining movies left are:"
+        puts
+        movies.each do |movie, rating|
+            puts "#{movie}: #{rating}"
+        end
+    end
+
 else
-  puts "Sorry, I didn't understand you."
+    puts "Error!"
 end
